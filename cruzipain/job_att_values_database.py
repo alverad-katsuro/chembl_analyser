fdc4911cb107eb694114d3bc5c33562319b6ae11
Alfredo Gabriel <amgabriel1@hotmail.com>
2022-02-08 12:49:34 -0300

    att job

--- cruzipain/job_att_values_database.py
+++ cruzipain/job_att_values_database.py
@@ -9,36 +9,16 @@ sys.path.append(os.path.abspath("../module"))
 from lipinski import * 
 
 # %%
-pd.options.display.max_columns = 30
+con = sqlite3.connect("../data/propriedades.db")
 
 # %%
-con = sqlite3.connect("../data/chembl_29.db")
+molregno_chembl_id = pd.read_sql_query("select * from dados", con)
 
 # %%
-molregno_chembl_id = pd.read_sql_query("SELECT compound_properties.*, molecule_dictionary.chembl_id, compound_structures.canonical_smiles FROM compound_properties INNER JOIN compound_structures ON compound_structures.molregno=compound_properties.molregno INNER JOIN molecule_dictionary ON molecule_dictionary.molregno=compound_properties.molregno;", con)
-
 # %%
-molregno_chembl_id = molregno_chembl_id.drop(['ro3_pass', 'hba', 'hbd', 'num_ro5_violations', 'full_molformula'], axis=1)
-
-# %%
-molregno_chembl_id = pd.merge(molregno_chembl_id.chembl_id, molregno_chembl_id)
-
-# %%
-molregno_chembl_id = molregno_chembl_id.drop(['molregno'], axis=1)
-
-# %%
-index_duplicates = molregno_chembl_id[molregno_chembl_id.canonical_smiles.duplicated()].sort_values(by=['canonical_smiles']).index
-
-# %%
-molregno_chembl_id = molregno_chembl_id.drop(labels=index_duplicates, axis=0)
-molregno_chembl_id
-
-# %%
-ids_com_nan = molregno_chembl_id.loc[molregno_chembl_id.isnull().any(axis=1)][['chembl_id', 'canonical_smiles']]
-
-# %%
-ids_com_nan
+ids_com_nan = molregno_chembl_id[['chembl_id', 'canonical_smiles']]
 
 # %%
 
-chama_atualiza_in_sql(ids_com_nan, molregno_chembl_id, "dados", '../data/dados_att', 6*3, 6)
+procs = 20
+chama_atualiza_in_sql(ids_com_nan, molregno_chembl_id, "dados", '../data/dados_att', procs, procs)
